f0117b9807184ab7f64b886a108b3f032b40c215
edster <edsters88@hotmail.com>
2018-10-25 15:02:46 -0700

    forgot to remove debugging

--- resources/js/tags.js
+++ resources/js/tags.js
@@ -4,7 +4,6 @@
         document.querySelectorAll('select[data-provides="anomaly.field_type.multiple"]')
     );
     fields.forEach(function (field) {
-        console.log(field.getAttribute('data-searchResultLimit'));
         new Choices(field, {
             removeItemButton: true,
             searchResultLimit: field.getAttribute('data-searchResultLimit')
