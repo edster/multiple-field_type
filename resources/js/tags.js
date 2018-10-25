1184024c2d22cb8add7a4488b5c822d5576034d0
edster <edsters88@hotmail.com>
2018-10-25 15:01:19 -0700

    Add search result option

--- resources/js/tags.js
+++ resources/js/tags.js
@@ -3,10 +3,11 @@
     let fields = Array.prototype.slice.call(
         document.querySelectorAll('select[data-provides="anomaly.field_type.multiple"]')
     );
-
     fields.forEach(function (field) {
+        console.log(field.getAttribute('data-searchResultLimit'));
         new Choices(field, {
             removeItemButton: true,
+            searchResultLimit: field.getAttribute('data-searchResultLimit')
         });
     });
 })(window, document);
